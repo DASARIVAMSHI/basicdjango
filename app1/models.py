19d630feb8603c2a34679a3a0329473a4bc4bbc2
D P Naidu <190330272@klh.edu.in>
2021-03-30 22:48:41 +0530

    basic django

---
+++ app1/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+class student(models.Model):
+    first_name=models.CharField(max_length=20)
+    last_name=models.CharField(max_length=30)
+    contact=models.IntegerField()
+    email=models.EmailField(max_length=50)
+    age=models.IntegerField()
+
+
+
+
+
+
